89c033a1a1d6bc5cf6a568240adcea0b37012b9c
azeroth <jefry43429@gmail.com>
2023-05-18 08:49:43 -0600

    act

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import "./App.css";
 import Buttons from "./components/Buttons";
-import react, { usestate } from 'react';
-import React, { Component } from 'react'
-import ReactHowler from 'react-howler'
+import react, { usestate } from "react";
+import React, { Component } from "react";
+import ReactHowler from "react-howler";
 
 function App() {
   return (
     <div
-      className=" min-width: 640px min-w-[100vh] min-h-[100vh] bg-gradient-to-r from-yellow-300 via-purple-400 to-blue-400 
+      className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl max-w-screen-md min-w-[100vh] min-h-[100vh] bg-gradient-to-r from-yellow-300 via-purple-400 to-blue-400 
      "
     >
+
+<ReactHowler
+        src={require("../src/sound/track.mp3")}
+        playing={true}
+      ></ReactHowler>
+      ;
       <div className="  p-48 ml-16 grid  grid-flow-col ">
-      < ReactHowler 
-        src = 'http://goldfirestudios.com/proj/howlerjs/sound.ogg' 
-        playing = { true } />
-       
         <Buttons className=" float-left">Next</Buttons>
       </div>
     </div>
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -4,12 +4,18 @@ import React, { useState } from 'react';
 function Buttons() {
   
   const listImg=[
-    "",
+    "https://i.pinimg.com/originals/a4/e8/5a/a4e85aada79e7f2e901a7eec69de2cc8.jpg",
     "https://www.elcato.org/sites/default/files/images/stories/el-principito.jpg",
     "https://steamuserimages-a.akamaihd.net/ugc/1460808097036933622/004E7D219B42C10DFDBE43837EAEB100476A7E98/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
     "https://w0.peakpx.com/wallpaper/683/867/HD-wallpaper-zelda-and-link.jpg",
     "https://e1.pxfuel.com/desktop-wallpaper/738/126/desktop-wallpaper-tanjiro-x-nezuko-nezuko-thumbnail.jpg",
     "http://oyster.ignimgs.com/wordpress/stg.ign.com/2015/03/ori_030715_1600.jpg",
+    "https://i0.wp.com/gamelx.es/wp-content/uploads/2021/01/thumb-1920-121645.jpg?resize=1024%2C640&ssl=1",
+    "https://e1.pxfuel.com/desktop-wallpaper/88/715/desktop-wallpaper-cloud-strife-and-tifa-lockhart-final-fantasy-cloud-tifa.jpg",
+    "https://media.nichegamer.com/wp-content/uploads/2021/07/monster-hunter-stories-2-wings-of-ruin-7-10-2021-1.jpg",
+    "https://wallpaperaccess.com/full/3300037.png",
+    "https://e1.pxfuel.com/desktop-wallpaper/880/301/desktop-wallpaper-100-gochi-trending-gochi.jpg",
+
     
   ]
 
